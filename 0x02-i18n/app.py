b52f9a7cd949249365b4fe65a01de1e706d9b0ec
HenryAnyiam <anyiamhenry7@gmail.com>
2024-01-10 00:13:15 +0100

    added timezone property

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+"""A simple flask app"""
+
+
+import pytz
+from flask import Flask, render_template, g, request
+from flask_babel import Babel, format_datetime
+from datetime import datetime
+
+
+class Config:
+    """config class for flask app"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """get user id from request"""
+    user = request.args.get('login_as')
+
+    if user:
+        try:
+            user = int(user)
+        except ValueError:
+            pass
+        else:
+            user = users.get(user)
+            if user:
+                return user
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.before_request
+def before_request():
+    """handle before all other functions"""
+    user = get_user()
+    setattr(g, 'user', user)
+
+
+def get_locale():
+    """verifies users locale"""
+    user_locale = request.args.get('locale')
+    user = getattr(g, 'user', None)
+
+    if user_locale is not None and user_locale in app.config["LANGUAGES"]:
+        return user_locale
+    elif user and user["locale"] in app.config["LANGUAGES"]:
+        return user["locale"]
+
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+def get_timezone():
+    """verify user timezone"""
+    user_time = request.args.get('timezone')
+    user = getattr(g, 'user', None)
+
+    if user_time:
+        try:
+            pytz.timezone(user_time)
+            return user_time
+        except pytz.UnknownTimeZoneError:
+            pass
+    elif user:
+        return user["timezone"]
+    return app.config["BABEL_DEFAULT_TIMEZONE"]
+
+
+
+babel = Babel(app, locale_selector=get_locale, timezone_selector=get_timezone)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    date = datetime.now()
+    f_date = format_datetime(date, format='medium')
+    return render_template("index.html", user=getattr(g, 'user', None), date=f_date)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
